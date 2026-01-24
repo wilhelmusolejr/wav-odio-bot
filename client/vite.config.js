fb5872b54b23f0aff80b804ca004f8a74863db82
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2026-01-25 02:07:22 +0800

    vite config add allowed page.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["zechariah-interspinal-delayingly.ngrok-free.dev"],
     host: "192.168.177.251",
     port: 5173,
   },
