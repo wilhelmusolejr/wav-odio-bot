b1f82be80c679150bfc5b78b588b9661d4f73ef5
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2026-01-25 02:04:09 +0800

    simple acount js

---
+++ server/account.js
@@ -0,0 +1,42 @@
+[
+  {
+    username: "botfrag666",
+    voice_type: "real_brendan666",
+    noises: "none",
+  },
+  {
+    username: "jeroam",
+    voice_type: "ai_kael",
+    noises: "white_noise",
+  },
+  {
+    username: "echogreg",
+    voice_type: "real_brendan666",
+    noises: "fan",
+  },
+  {
+    username: "jennyhums30",
+    voice_type: "",
+    noises: "none",
+  },
+  {
+    username: "elooo2092",
+    voice_type: "real_brendan666",
+    noises: "white_noise",
+  },
+  {
+    username: "g3ooorge",
+    voice_type: "",
+    noises: "white_noise",
+  },
+  {
+    username: "koooooalaid",
+    voice_type: "real_brendan666",
+    noises: "none",
+  },
+  {
+    username: "totoyoymonaxia",
+    voice_type: "ai_kael",
+    noises: "fan",
+  },
+];
